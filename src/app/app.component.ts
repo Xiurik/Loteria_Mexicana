640e427c97492f9b01e88b6f07aed1d5613ae3c4
Irwin Romero <irwin.romero.rdz@gmail.com>
2023-04-25 14:17:38 -0600

    Update Cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,67 +8,72 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   //#region 'Variables'
   loteria = [
-    [1, 'gallo', './assets/images/1.jpg'],
-    [2, 'diablito', './assets/images/2.jpg'],
-    [3, 'dama', './assets/images/3.jpg'],
-    [4, 'catrin', './assets/images/4.jpg'],
-    [5, 'paraguas', './assets/images/5.jpg'],
-    [6, 'sirena', './assets/images/6.jpg'],
-    [7, 'escalera', './assets/images/7.jpg'],
-    [8, 'botella', './assets/images/8.jpg'],
-    [9, 'barril', './assets/images/9.jpg'],
-    [10, 'arbol', './assets/images/10.jpg'],
-    [11, 'melon', './assets/images/11.jpg'],
-    [12, 'valiente', './assets/images/12.jpg'],
-    [13, 'gorrito', './assets/images/13.jpg'],
-    [14, 'muerte', './assets/images/14.jpg'],
-    [15, 'pera', './assets/images/15.jpg'],
-    [16, 'bandera', './assets/images/16.jpg'],
-    [17, 'bandolon', './assets/images/17.jpg'],
-    [18, 'violoncello', './assets/images/18.jpg'],
-    [19, 'garza', './assets/images/19.jpg'],
-    [20, 'pajaro', './assets/images/20.jpg'],
-    [21, 'mano', './assets/images/21.jpg'],
-    [22, 'bota', './assets/images/22.jpg'],
-    [23, 'luna', './assets/images/23.jpg'],
-    [24, 'cotorro', './assets/images/24.jpg'],
-    [25, 'borracho', './assets/images/25.jpg'],
-    [26, 'negrito', './assets/images/26.jpg'],
-    [27, 'corazon', './assets/images/27.jpg'],
-    [28, 'sandia', './assets/images/28.jpg'],
-    [29, 'tambor', './assets/images/29.jpg'],
-    [30, 'camaron', './assets/images/30.jpg'],
-    [31, 'jaras', './assets/images/31.jpg'],
-    [32, 'musico', './assets/images/32.jpg'],
-    [33, 'arania', './assets/images/33.jpg'],
-    [34, 'soldado', './assets/images/34.jpg'],
-    [35, 'estrella', './assets/images/35.jpg'],
-    [36, 'cazo', './assets/images/36.jpg'],
-    [37, 'mundo', './assets/images/37.jpg'],
-    [38, 'apache', './assets/images/38.jpg'],
-    [39, 'nopal', './assets/images/39.jpg'],
-    [40, 'alacran', './assets/images/40.jpg'],
-    [41, 'rosa', './assets/images/41.jpg'],
-    [42, 'calavera', './assets/images/42.jpg'],
-    [43, 'campana', './assets/images/43.jpg'],
-    [44, 'cantarito', './assets/images/44.jpg'],
-    [45, 'venado', './assets/images/45.jpg'],
-    [46, 'sol', './assets/images/46.jpg'],
-    [47, 'corona', './assets/images/47.jpg'],
-    [48, 'chalupa', './assets/images/48.jpg'],
-    [49, 'pino', './assets/images/49.jpg'],
-    [50, 'pescado', './assets/images/50.jpg'],
-    [51, 'palma', './assets/images/51.jpg'],
-    [52, 'maceta', './assets/images/52.jpg'],
-    [53, 'arpa', './assets/images/53.jpg'],
-    [54, 'rana', './assets/images/54.jpg'],
+    [1, 'gallo', './assets/images/cartas/1.png'],
+    [2, 'diablito', './assets/images/cartas/2.png'],
+    [3, 'dama', './assets/images/cartas/3.png'],
+    [4, 'catrin', './assets/images/cartas/4.png'],
+    [5, 'paraguas', './assets/images/cartas/5.png'],
+    [6, 'sirena', './assets/images/cartas/6.png'],
+    [7, 'escalera', './assets/images/cartas/7.png'],
+    [8, 'botella', './assets/images/cartas/8.png'],
+    [9, 'barril', './assets/images/cartas/9.png'],
+    [10, 'arbol', './assets/images/cartas/10.png'],
+    [11, 'melon', './assets/images/cartas/11.png'],
+    [12, 'valiente', './assets/images/cartas/12.png'],
+    [13, 'gorrito', './assets/images/cartas/13.png'],
+    [14, 'muerte', './assets/images/cartas/14.png'],
+    [15, 'pera', './assets/images/cartas/15.png'],
+    [16, 'bandera', './assets/images/cartas/16.png'],
+    [17, 'bandolon', './assets/images/cartas/17.png'],
+    [18, 'violoncello', './assets/images/cartas/18.png'],
+    [19, 'garza', './assets/images/cartas/19.png'],
+    [20, 'pajaro', './assets/images/cartas/20.png'],
+    [21, 'mano', './assets/images/cartas/21.png'],
+    [22, 'bota', './assets/images/cartas/22.png'],
+    [23, 'luna', './assets/images/cartas/23.png'],
+    [24, 'cotorro', './assets/images/cartas/24.png'],
+    [25, 'borracho', './assets/images/cartas/25.png'],
+    [26, 'negrito', './assets/images/cartas/26.png'],
+    [27, 'corazon', './assets/images/cartas/27.png'],
+    [28, 'sandia', './assets/images/cartas/28.png'],
+    [29, 'tambor', './assets/images/cartas/29.png'],
+    [30, 'camaron', './assets/images/cartas/30.png'],
+    [31, 'jaras', './assets/images/cartas/31.png'],
+    [32, 'musico', './assets/images/cartas/32.png'],
+    [33, 'arania', './assets/images/cartas/33.png'],
+    [34, 'soldado', './assets/images/cartas/34.png'],
+    [35, 'estrella', './assets/images/cartas/35.png'],
+    [36, 'cazo', './assets/images/cartas/36.png'],
+    [37, 'mundo', './assets/images/cartas/37.png'],
+    [38, 'apache', './assets/images/cartas/38.png'],
+    [39, 'nopal', './assets/images/cartas/39.png'],
+    [40, 'alacran', './assets/images/cartas/40.png'],
+    [41, 'rosa', './assets/images/cartas/41.png'],
+    [42, 'calavera', './assets/images/cartas/42.png'],
+    [43, 'campana', './assets/images/cartas/43.png'],
+    [44, 'cantarito', './assets/images/cartas/44.png'],
+    [45, 'venado', './assets/images/cartas/45.png'],
+    [46, 'sol', './assets/images/cartas/46.png'],
+    [47, 'corona', './assets/images/cartas/47.png'],
+    [48, 'chalupa', './assets/images/cartas/48.png'],
+    [49, 'pino', './assets/images/cartas/49.png'],
+    [50, 'pescado', './assets/images/cartas/50.png'],
+    [51, 'palma', './assets/images/cartas/51.png'],
+    [52, 'maceta', './assets/images/cartas/52.png'],
+    [53, 'arpa', './assets/images/cartas/53.png'],
+    [54, 'rana', './assets/images/cartas/54.png'],
   ];
 
   public card: any[] = [];
   public cards: any[] = [];
   public cards_ordered: any[] = [];
 
-  public name_top = 'Frida Sofia Lopez Rodriguez';
+  public name_top = '';
+  public dobles = 'true';
+  public cuantas = '0';
+  public cantidad = '';
+  public uibicacion = '666';
+  public ubicacionDoblesIMG = './assets/images/orden/666.png';
   //#endregion 'Variables'
 
   //#region 'Angular LifeCycle'
@@ -76,11 +81,15 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.create_cards();
-    // this.create_specific_cards();
   }
   //#endregion 'Angular LifeCycle'
 
   //#region 'General Methods'
+
+  public ubicacionDobles(): void {
+    this.ubicacionDoblesIMG = `./assets/images/orden/${this.uibicacion}.png`;
+  }
+
   private get_random(min: number, max: number, invalid: number) {
     let random = Math.floor(Math.random() * (max - min + 1) + min);
     let exist = this.card.filter((obj) => obj[0] === random);
@@ -91,32 +100,9 @@ export class AppComponent implements OnInit {
     return random;
   }
 
-  private get_min_max(index: number) {
-    let min = 0;
-    let max = 0;
-
-    if (index == 1 || index == 5 || index == 9 || index == 13) {
-      min = 1;
-      max = 14;
-    } else if (index == 2 || index == 6 || index == 10 || index == 14) {
-      min = 15;
-      max = 28;
-    } else if (index == 3 || index == 7 || index == 11 || index == 15) {
-      min = 29;
-      max = 36;
-    } else {
-      min = 37;
-      max = 54;
-    }
-
-    return { min, max };
-  }
-
   private create_card(invalid: number) {
     this.card = [];
     for (let i = 1; i < 16; i++) {
-      // let min_max = this.get_min_max(i);
-      // let random = this.get_random(min_max.min, min_max.max, invalid);
       let random = this.get_random(1, 54, invalid);
       let lote = this.loteria.filter((obj) => obj[0] === random)[0];
       if (i < 15) {
